7d0d65e5dbce0e1678c155b728a9fa153ab7b9f5
Lee Anderson <ecllips07@gmail.com>
2021-08-27 10:30:34 -0600

    saved

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_25_233327) do
+ActiveRecord::Schema.define(version: 2021_08_26_200740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "items", force: :cascade do |t|
+  create_table "departments", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.bigint "department_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["department_id"], name: "index_items_on_department_id"
   end
 
   create_table "stores", force: :cascade do |t|
@@ -27,4 +35,5 @@ ActiveRecord::Schema.define(version: 2021_08_25_233327) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "items", "departments"
 end
